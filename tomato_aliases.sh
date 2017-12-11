262755a4603766b8762c9675fb1de85f2e7f0ef7
A.M. Thomas <thomasoflight@lightfaced.org>
2017-12-11 13:53:52 -0500

    Initial commit

---
+++ tomato_aliases.sh
@@ -0,0 +1,20 @@
+# APPLESCRIPTS
+alias fade_to_black='osascript /Users/$USER/L_LGHTREE/LL_SOFT/APPLESCRIPTS/fade_to_black.scpt' 
+alias fade_to_mid='osascript /Users/$USER/L_LGHTREE/LL_SOFT/APPLESCRIPTS/fade_to_mid.scpt'
+
+# TIMERS
+alias t3='sleep 180'
+alias t4='sleep 300'
+alias t8='sleep 480'
+alias t12='sleep 720'
+alias t15='sleep 950; say "Ding ding ding"'
+alias t20='sleep 1200'
+
+alias t_15='t15; fade_to_black; t4; fade_to_mid; iterm'
+alias t_20='t20; fade_to_black; t4; fade_to_mid; iterm'
+alias t_rounds_3='t_15; t_15; t_15;'
+
+
+# set a timer
+# set a break
+# set a number of rounds
